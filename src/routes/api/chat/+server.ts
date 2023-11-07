88ed4a407c9a59d93e590b9d27dec71f896941fc
LordLuceus <lord.luceus@gmail.com>
2023-11-07 17:57:33 +0100

    feat: Add the AI description feature

---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,38 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { OPENAI_API_KEY } from "$env/static/private";
+import type { RequestHandler } from "@sveltejs/kit";
+import type { ChatCompletionMessageParam } from "openai/resources";
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+export const POST = (async ({ request }) => {
+  const { imageUrl } = await request.json();
+  const messages: ChatCompletionMessageParam[] = [
+    {
+      role: "system",
+      content:
+        "You are a helpful AI assistant that describes the artwork of Hearthstone cards for blind and visually impaired players. Your descriptions should be as detailed and accurate as possible. You will be given a card image, and you should describe the card art in as much detail as possible. You do not need to describe the card's text or gameplay mechanics. Describe only what is shown in the card art; do not make up any details that are not shown in the card art."
+    },
+    {
+      role: "user",
+      content: [
+        {
+          type: "image_url",
+          image_url: { url: imageUrl }
+        }
+      ]
+    }
+  ];
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    stream: true,
+    messages,
+    max_tokens: 3000
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const stream = OpenAIStream(response as AsyncIterable<any>);
+
+  return new StreamingTextResponse(stream);
+}) satisfies RequestHandler;
--- src/routes/describe/[cardId]/+page.svelte
+++ src/routes/describe/[cardId]/+page.svelte
@@ -1,7 +1,21 @@
 <script>
+  import { useChat } from "ai/svelte";
+  import { onMount } from "svelte";
   import CardDetails from "./CardDetails.svelte";
 
   export let data;
+
+  const { append, messages } = useChat({
+    body: { imageUrl: data.card?.img },
+    initialInput:
+      "You are a helpful AI assistant that describes the artwork of Hearthstone cards for blind and visually impaired players. Your descriptions should be as detailed and accurate as possible. You will be given a card image, and you should describe the card art in as much detail as possible. You do not need to describe the card's text or gameplay mechanics. Describe only what is shown in the card art; do not make up any details that are not shown in the card art."
+  });
+
+  onMount(() => {
+    if (data.card) {
+      append({ content: "Describe this card.", role: "user" });
+    }
+  });
 </script>
 
 <svelte:head>
@@ -16,6 +30,12 @@
 <main>
   {#if data.card}
     <CardDetails card={data.card} />
+    <h2>Description</h2>
+    {#each $messages as message}
+      {#if message.role === "assistant"}
+        <p class="message">{message.content}</p>
+      {/if}
+    {/each}
   {:else}
     <p>Card details will appear here.</p>
   {/if}
