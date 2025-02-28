296f0b72de1586e0da6699ef2ac1715ef0189f85
LordLuceus <lord.luceus@gmail.com>
2025-02-28 10:08:19 +0100

    chore: Add deploy workflow

--- src/lib/redis/index.ts
+++ src/lib/redis/index.ts
@@ -2,7 +2,7 @@ import { env } from "$env/dynamic/private";
 import IoRedis from "ioredis";
 
 export const redis = new IoRedis({
-  host: "localhost",
+  host: "valkey-server",
   port: 6379,
   password: env.REDIS_PASSWORD
 });
