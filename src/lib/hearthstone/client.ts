f921bdbb23f805cefd0ac6f63d58e0d73fce5481
LordLuceus <lord.luceus@gmail.com>
2025-02-28 10:22:49 +0100

    fix: Use dynamic env for hearthstone client

--- src/lib/hearthstone/client.ts
+++ src/lib/hearthstone/client.ts
@@ -1,9 +1,9 @@
-import { BATTLENET_CLIENT_ID, BATTLENET_CLIENT_SECRET } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { hs } from "blizzard.js";
 
 export const hearthstoneClient = await hs.createInstance({
-  key: BATTLENET_CLIENT_ID,
-  secret: BATTLENET_CLIENT_SECRET,
+  key: env.BATTLENET_CLIENT_ID,
+  secret: env.BATTLENET_CLIENT_SECRET,
   origin: "us",
   locale: "en_US"
 });
