ef391a30acb63d5e287ef6e405bcb1e4261162da
LordLuceus <lord.luceus@gmail.com>
2025-02-28 10:43:13 +0100

    fix: Check if hs variables exist before instantiating client

--- src/lib/hearthstone/client.ts
+++ src/lib/hearthstone/client.ts
@@ -1,9 +1,17 @@
 import { env } from "$env/dynamic/private";
 import { hs } from "blizzard.js";
+import type { HSClient } from "blizzard.js/dist/hs";
 
-export const hearthstoneClient = await hs.createInstance({
-  key: env.BATTLENET_CLIENT_ID,
-  secret: env.BATTLENET_CLIENT_SECRET,
-  origin: "us",
-  locale: "en_US"
-});
+async function getHSClient() {
+  if (!env.BATTLENET_CLIENT_ID || !env.BATTLENET_CLIENT_SECRET) {
+    return null;
+  }
+  return await hs.createInstance({
+    key: env.BATTLENET_CLIENT_ID,
+    secret: env.BATTLENET_CLIENT_SECRET,
+    origin: "us",
+    locale: "en_US"
+  });
+}
+
+export const hearthstoneClient = (await getHSClient()) as HSClient;
